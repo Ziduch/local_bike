bd065ff2ddb3df07e4c195f30ae6ae68fcba5bf3
mathieu devise <mathieu.devise@gmail.com>
2026-02-16 22:12:30 +0000

    UPD ajout des noms de magasins + maj doc

--- models/mart/mrt_local_bike__orders_daily_store.sql
+++ models/mart/mrt_local_bike__orders_daily_store.sql
@@ -1,11 +1,14 @@
 select
-  date_trunc(order_date, day) as report_date,
-  store_id,
-  count(distinct order_id) as total_orders,
-  sum(total_order_amount) as total_revenue,
-  avg(total_order_amount) as avg_order_value,
-  sum(total_items) as total_items,
-  avg(total_distinct_products) as avg_distinct_products,
-  avg(avg_discount) as avg_discount
-from {{ ref('int_local_bike__order_summary') }}
-group by 1,2
+  date_trunc(o.order_date, day) as report_date,
+  o.store_id,
+  s.store_name,
+  count(distinct o.order_id) as total_orders,
+  sum(o.total_order_amount) as total_revenue,
+  avg(o.total_order_amount) as avg_order_value,
+  sum(o.total_items) as total_items,
+  avg(o.total_distinct_products) as avg_distinct_products,
+  avg(o.avg_discount) as avg_discount
+from {{ ref('int_local_bike__order_summary') }} o
+left join {{ ref('bronze_stores') }} s
+  on o.store_id = s.store_id
+group by 1,2,3
--- models/mart/mrt_local_bike__orders_monthly_store.sql
+++ models/mart/mrt_local_bike__orders_monthly_store.sql
@@ -1,8 +1,11 @@
 select
   date_trunc(order_date, month) as report_month,
-  store_id,
+  o.store_id,
+  s.store_name,
   count(distinct order_id) as total_orders,
   sum(total_order_amount) as total_revenue,
   avg(total_order_amount) as avg_order_value
-from {{ ref('int_local_bike__order_summary') }}
-group by 1,2
+from {{ ref('int_local_bike__order_summary') }} o
+left join {{ ref('bronze_stores') }} s
+  on o.store_id = s.store_id
+group by 1,2,3
